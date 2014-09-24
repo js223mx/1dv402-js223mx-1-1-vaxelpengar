b232690177d300308dfb48b43060e3c4bfde0aa7
unknown <js223mx@student.lnu.se>
2014-09-24 14:44:09 +0200

    approved by teacher

--- 1-1-vaxelpengar/Program.cs
+++ 1-1-vaxelpengar/Program.cs
@@ -12,39 +12,37 @@ namespace _1_1_vaxelpengar
         {
 
             //Ange variabel för totalsumman samt erhållet belopp
-            double totalsum; 
+            double totalsum;
             uint amountReceived;
-            
+
 
             while (true)
             {
                 try
-               {
-                Console.Write("Vänligen ange belopp för köp: ");
-                totalsum=double.Parse(Console.ReadLine());
-
-                if (totalsum >0)
                 {
-                    break;
-                }
+                    Console.Write("Vänligen ange belopp för köp: ");
+                    totalsum = double.Parse(Console.ReadLine());
 
+                    if (totalsum > 0)
+                    {
+                        break;
+                    }
                     else //fångar fel inmatning
-                {
-                    Console.BackgroundColor=ConsoleColor.Red;
-                    Console.Write("FEL! Vänligen ange giltigt belopp.");
-                    Console.ResetColor();
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.Write("FEL! Vänligen ange giltigt belopp.");
+                        Console.ResetColor();
+                    }
                 }
-               }
-                
-                   catch //fångar fel inmatning av typen string
-                   {
-                    Console.BackgroundColor=ConsoleColor.Red;
+                catch //fångar fel inmatning av typen string
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
                     Console.Write("FEL! Ogiltig inmatning, vänligen ange giltigt belopp.");
                     Console.ResetColor();
-                   }
+                }
             }
 
-uint subTotalPrice = (uint)Math.Round(totalsum);
+            uint subTotalPrice = (uint)Math.Round(totalsum);
 
             while (true)
             {
@@ -57,7 +55,6 @@ uint subTotalPrice = (uint)Math.Round(totalsum);
                     {
                         break;
                     }
-
                     else //fångar fel inmatning  
                     {
                         Console.BackgroundColor = ConsoleColor.Red;
@@ -65,7 +62,6 @@ uint subTotalPrice = (uint)Math.Round(totalsum);
                         Console.ResetColor();
                     }
                 }
-
                 catch //fångar fel intmatning av typen string
                 {
                     Console.BackgroundColor = ConsoleColor.Red;
@@ -73,8 +69,8 @@ uint subTotalPrice = (uint)Math.Round(totalsum);
                     Console.ResetColor();
                 }
             }
-            
-            
+
+
             double roundingOffAmount = subTotalPrice - totalsum;
             uint amountBack = amountReceived - subTotalPrice;
 
@@ -86,7 +82,7 @@ uint subTotalPrice = (uint)Math.Round(totalsum);
             cash = cash % 50;
             uint twenty = cash / 20;
             cash = cash % 20;
-            uint ten = cash /10;
+            uint ten = cash / 10;
             cash = cash % 10;
             uint five = cash / 5;
             cash = cash % 5;
@@ -106,22 +102,22 @@ uint subTotalPrice = (uint)Math.Round(totalsum);
             Console.WriteLine("-----------------------------------");
 
             //tala om hur mycket av varje peng som ska tillbaka
-
-            if (fivehundred > 0);
-            Console.WriteLine("500-lappar          : {0}", fivehundred);
-            if (hundred > 0);
-            Console.WriteLine("100-lappar          : {0}", hundred);
-            if (fifty > 0);
-            Console.WriteLine("50-lappar           : {0}", fifty);
-            if (twenty > 0);
-            Console.WriteLine("20-lappar           : {0}", twenty);
-            if (ten > 0);
-            Console.WriteLine("10-kronor           : {0}", ten);
-            if (five > 0);
-            Console.WriteLine("5-kronor            : {0}", five);
-            if (one > 0);
-            Console.WriteLine("1-kronor            : {0}", one);   
-
+            if (fivehundred > 0)
+            {
+                Console.WriteLine("500-lappar          : {0}", fivehundred);
+            }
+            if (hundred > 0)
+                Console.WriteLine("100-lappar          : {0}", hundred);
+            if (fifty > 0)
+                Console.WriteLine("50-lappar           : {0}", fifty);
+            if (twenty > 0)
+                Console.WriteLine("20-lappar           : {0}", twenty);
+            if (ten > 0)
+                Console.WriteLine("10-kronor           : {0}", ten);
+            if (five > 0)
+                Console.WriteLine("5-kronor            : {0}", five);
+            if (one > 0)
+                Console.WriteLine("1-kronor            : {0}", one);
         }
     }
 }
